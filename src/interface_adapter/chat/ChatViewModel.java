3a0cd5f439b81f2ee5b1e3c676e01c53f56a97c5
Justus Croskery <justus.croskery@gmail.com>
2023-11-19 17:10:13 -0500

    Make ChatViewModel implement ViewModel

--- src/interface_adapter/chat/ChatViewModel.java
+++ src/interface_adapter/chat/ChatViewModel.java
@@ -1,15 +1,18 @@
 package interface_adapter.chat;
 
 import entity.Message;
+import interface_adapter.ViewModel;
+
 import java.beans.PropertyChangeSupport;
 import java.util.List;
 
-public class ChatViewModel {
+public class ChatViewModel extends ViewModel {
   private final PropertyChangeSupport support = new PropertyChangeSupport(this);
 
   List<Message> messages;
 
   public ChatViewModel(List<Message> messages) {
+    super("chat");
     this.messages = messages;
   }
 
