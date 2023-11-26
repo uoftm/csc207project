7fb6bb9ef2aabf57e71a6a134a3042e71dc2a0cc
Milo <modderme123+school@gmail.com>
2023-11-26 15:54:35 -0500

    cleanup searchtest

--- tests/SearchTest.java
+++ tests/SearchTest.java
@@ -8,13 +8,12 @@ import okhttp3.OkHttpClient;
 import org.junit.Test;
 
 public class SearchTest {
-  private SearchDataAccessObject searchDataAccessObject;
-
   private final OkHttpClient okHttpClient = new OkHttpClient();
+  private final SearchDataAccessObject searchDataAccessObject =
+      new SearchDataAccessObject(okHttpClient);
 
   @Test
   public void saveData() {
-    SearchDataAccessObject searchDataAccessObject = new SearchDataAccessObject(okHttpClient);
     SearchChatMessage searchMessage =
         new SearchChatMessage(Instant.now(), "test-room-id", "test-message");
     searchDataAccessObject.saveData(searchMessage);
@@ -22,7 +21,6 @@ public class SearchTest {
 
   @Test
   public void testGetData() {
-    SearchDataAccessObject searchDataAccessObject = new SearchDataAccessObject(okHttpClient);
     SearchRequest searchRequest = new SearchRequest("dog", Instant.now(), "2");
     assertEquals(
         searchDataAccessObject.getData(searchRequest).getResponses().getFirst().getFullText(),
