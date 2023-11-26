2f45ffcc76557d510603a34ae36d922b328f8e5e
Milo <modderme123+school@gmail.com>
2023-11-25 20:30:07 -0500

    format tests

--- tests/SearchTest.java
+++ tests/SearchTest.java
@@ -1,26 +1,22 @@
+import static org.junit.Assert.*;
+
 import data_access.SearchDataAccessObject;
 import entity.SearchRequest;
+import java.time.Instant;
 import okhttp3.OkHttpClient;
 import org.junit.Test;
 
-import java.time.Instant;
-
-import static org.junit.Assert.*;
-
-
-
 public class SearchTest {
-    private SearchDataAccessObject searchDataAccessObject;
+  private SearchDataAccessObject searchDataAccessObject;
 
-    private final OkHttpClient okHttpClient = new OkHttpClient();
+  private final OkHttpClient okHttpClient = new OkHttpClient();
 
-    @Test
-    public void testGetData() {
-        SearchDataAccessObject searchDataAccessObject = new SearchDataAccessObject(okHttpClient);
-        SearchRequest searchRequest = new SearchRequest("dog", Instant.now(), "2");
-        assertEquals(searchDataAccessObject.getData(searchRequest).getResponses().getFirst().getFullText(), "I love dogs" );
-    }
-
-
-
+  @Test
+  public void testGetData() {
+    SearchDataAccessObject searchDataAccessObject = new SearchDataAccessObject(okHttpClient);
+    SearchRequest searchRequest = new SearchRequest("dog", Instant.now(), "2");
+    assertEquals(
+        searchDataAccessObject.getData(searchRequest).getResponses().getFirst().getFullText(),
+        "I love dogs");
+  }
 }
