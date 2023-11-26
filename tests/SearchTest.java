49c90f3a5697b8b9b7610886f503c75aace14e17
alex <Alex.wang1903@gmail.com>
2023-11-25 19:52:55 -0500

    completed testgetdata

--- tests/SearchTest.java
+++ tests/SearchTest.java
@@ -1,8 +1,23 @@
+import data_access.SearchDataAccessObject;
+import entity.SearchRequest;
+import org.junit.Test;
+
+import java.time.Instant;
+
 import static org.junit.Assert.*;
 
 
 
 public class SearchTest {
+    private SearchDataAccessObject searchDataAccessObject;
+
+    @Test
+    public void testGetData() {
+        SearchDataAccessObject searchDataAccessObject = new SearchDataAccessObject();
+        SearchRequest searchRequest = new SearchRequest("dog", Instant.now(), "2");
+        assertEquals(searchDataAccessObject.getData(searchRequest).getResponses().getFirst().getFullText(), "I love dogs" );
+    }
+
 
 
 }
