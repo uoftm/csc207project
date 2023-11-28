0be76fa97587130984b1716dd270c0507fef930b
Alex <alexanderrosen45@gmail.com>
2023-11-28 00:47:33 -0500

    Cleanup login/singup/welcome view

--- src/view/ViewConstants.java
+++ src/view/ViewConstants.java
@@ -3,8 +3,11 @@ package view;
 import java.awt.*;
 
 public class ViewConstants {
-  public static final Color background = Color.YELLOW;
-  public static final Color panel = Color.RED;
+  // Using a soft blue for the background for a calming effect
+  public static final Color background = new Color(210, 230, 250); // Light Blue
+
+  // Using a darker shade of blue for the panel to maintain harmony
+  public static final Color panel = new Color(153, 204, 255); // Steel Blue
 
   public static final Dimension windowSize = new Dimension(800, 600);
   public static final Dimension paneSize = new Dimension(400, 300);
--- src/view/WelcomeView.java
+++ src/view/WelcomeView.java
@@ -3,34 +3,67 @@ package view;
 import interface_adapter.switch_view.SwitchViewController;
 import java.awt.*;
 import javax.swing.*;
+import java.util.Random;
 
 public class WelcomeView {
-  public static final String viewName = "initial welcome";
+    public static final String viewName = "initial welcome";
 
-  public JPanel contentPane;
+    public JPanel contentPane;
+    private JLabel captionLabel; // This is now bound through the GUI designer
+    private JButton login;
+    private JButton signup;
+    private JPanel body;
 
-  private JButton login;
-  private JButton signup;
-  private JPanel body;
+    public WelcomeView(SwitchViewController switchViewController) {
+        contentPane.setBackground(ViewConstants.background);
+        contentPane.setPreferredSize(ViewConstants.windowSize);
+        body.setBackground(ViewConstants.panel);
+        body.setPreferredSize(ViewConstants.paneSize);
 
-  public WelcomeView(SwitchViewController switchViewController) {
-    contentPane.setBackground(ViewConstants.background);
-    contentPane.setPreferredSize(ViewConstants.windowSize);
-    body.setBackground(ViewConstants.panel);
-    body.setPreferredSize(ViewConstants.paneSize);
+        // Array of captions
+        String[] captions = {
+                "Connect Effortlessly, Communicate Seamlessly.",
+                "Where Conversations Come Alive.",
+                "Redefining Conversation in the Digital Age.",
+                "Speak Freely, Connect Deeply.",
+                "Bringing Your Words to Life.",
+                "Instant Messaging, Lasting Connections.",
+                "Talk the Talk, Anywhere, Anytime.",
+                "Your World, Closer with Every Message.",
+                "Elevate Your Chat Experience.",
+                "Chat Smarter, Not Harder.",
+                "Messages that Matter.",
+                "Beyond Texting: Experience the Difference.",
+                "Crafting Connections, One Message at a Time.",
+                "Unleash the Power of Conversation.",
+                "Speak Easy, Connect Instantly.",
+                "Where Messages Meet Magic.",
+                "Chat Revolutionized.",
+                "Feel Closer with Every Text.",
+                "Innovating How You Communicate.",
+                "The Art of Messaging, Redefined."
+        };
 
-    signup.addActionListener(
-        evt -> {
-          if (evt.getSource().equals(signup)) {
-            switchViewController.switchTo(SignupView.viewName);
-          }
-        });
+        // Randomly select a caption
+        Random rand = new Random();
+        String randomCaption = captions[rand.nextInt(captions.length)];
 
-    login.addActionListener(
-        evt -> {
-          if (evt.getSource().equals(login)) {
-            switchViewController.switchTo(LoginView.viewName);
-          }
-        });
-  }
+        captionLabel.setText(randomCaption);
+        captionLabel.setFont(new Font("Serif", Font.BOLD, 12));
+        captionLabel.setHorizontalAlignment(JLabel.CENTER);
+
+        signup.addActionListener(
+                evt -> {
+                    if (evt.getSource().equals(signup)) {
+                        switchViewController.switchTo(SignupView.viewName);
+                    }
+                });
+
+        login.addActionListener(
+                evt -> {
+                    if (evt.getSource().equals(login)) {
+                        switchViewController.switchTo(LoginView.viewName);
+                    }
+                });
+    }
 }
