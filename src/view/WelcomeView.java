e0b9a5005d5291f4048be67fc27b74bb78001c3a
Alex <alexanderrosen45@gmail.com>
2023-11-28 00:48:32 -0500

    Format view changes

--- src/view/WelcomeView.java
+++ src/view/WelcomeView.java
@@ -2,68 +2,68 @@ package view;
 
 import interface_adapter.switch_view.SwitchViewController;
 import java.awt.*;
-import javax.swing.*;
 import java.util.Random;
+import javax.swing.*;
 
 public class WelcomeView {
-    public static final String viewName = "initial welcome";
+  public static final String viewName = "initial welcome";
 
-    public JPanel contentPane;
-    private JLabel captionLabel; // This is now bound through the GUI designer
-    private JButton login;
-    private JButton signup;
-    private JPanel body;
+  public JPanel contentPane;
+  private JLabel captionLabel; // This is now bound through the GUI designer
+  private JButton login;
+  private JButton signup;
+  private JPanel body;
 
-    public WelcomeView(SwitchViewController switchViewController) {
-        contentPane.setBackground(ViewConstants.background);
-        contentPane.setPreferredSize(ViewConstants.windowSize);
-        body.setBackground(ViewConstants.panel);
-        body.setPreferredSize(ViewConstants.paneSize);
+  public WelcomeView(SwitchViewController switchViewController) {
+    contentPane.setBackground(ViewConstants.background);
+    contentPane.setPreferredSize(ViewConstants.windowSize);
+    body.setBackground(ViewConstants.panel);
+    body.setPreferredSize(ViewConstants.paneSize);
 
-        // Array of captions
-        String[] captions = {
-                "Connect Effortlessly, Communicate Seamlessly.",
-                "Where Conversations Come Alive.",
-                "Redefining Conversation in the Digital Age.",
-                "Speak Freely, Connect Deeply.",
-                "Bringing Your Words to Life.",
-                "Instant Messaging, Lasting Connections.",
-                "Talk the Talk, Anywhere, Anytime.",
-                "Your World, Closer with Every Message.",
-                "Elevate Your Chat Experience.",
-                "Chat Smarter, Not Harder.",
-                "Messages that Matter.",
-                "Beyond Texting: Experience the Difference.",
-                "Crafting Connections, One Message at a Time.",
-                "Unleash the Power of Conversation.",
-                "Speak Easy, Connect Instantly.",
-                "Where Messages Meet Magic.",
-                "Chat Revolutionized.",
-                "Feel Closer with Every Text.",
-                "Innovating How You Communicate.",
-                "The Art of Messaging, Redefined."
-        };
+    // Array of captions
+    String[] captions = {
+      "Connect Effortlessly, Communicate Seamlessly.",
+      "Where Conversations Come Alive.",
+      "Redefining Conversation in the Digital Age.",
+      "Speak Freely, Connect Deeply.",
+      "Bringing Your Words to Life.",
+      "Instant Messaging, Lasting Connections.",
+      "Talk the Talk, Anywhere, Anytime.",
+      "Your World, Closer with Every Message.",
+      "Elevate Your Chat Experience.",
+      "Chat Smarter, Not Harder.",
+      "Messages that Matter.",
+      "Beyond Texting: Experience the Difference.",
+      "Crafting Connections, One Message at a Time.",
+      "Unleash the Power of Conversation.",
+      "Speak Easy, Connect Instantly.",
+      "Where Messages Meet Magic.",
+      "Chat Revolutionized.",
+      "Feel Closer with Every Text.",
+      "Innovating How You Communicate.",
+      "The Art of Messaging, Redefined."
+    };
 
-        // Randomly select a caption
-        Random rand = new Random();
-        String randomCaption = captions[rand.nextInt(captions.length)];
+    // Randomly select a caption
+    Random rand = new Random();
+    String randomCaption = captions[rand.nextInt(captions.length)];
 
-        captionLabel.setText(randomCaption);
-        captionLabel.setFont(new Font("Serif", Font.BOLD, 12));
-        captionLabel.setHorizontalAlignment(JLabel.CENTER);
+    captionLabel.setText(randomCaption);
+    captionLabel.setFont(new Font("Serif", Font.BOLD, 12));
+    captionLabel.setHorizontalAlignment(JLabel.CENTER);
 
-        signup.addActionListener(
-                evt -> {
-                    if (evt.getSource().equals(signup)) {
-                        switchViewController.switchTo(SignupView.viewName);
-                    }
-                });
+    signup.addActionListener(
+        evt -> {
+          if (evt.getSource().equals(signup)) {
+            switchViewController.switchTo(SignupView.viewName);
+          }
+        });
 
-        login.addActionListener(
-                evt -> {
-                    if (evt.getSource().equals(login)) {
-                        switchViewController.switchTo(LoginView.viewName);
-                    }
-                });
-    }
+    login.addActionListener(
+        evt -> {
+          if (evt.getSource().equals(login)) {
+            switchViewController.switchTo(LoginView.viewName);
+          }
+        });
+  }
 }
