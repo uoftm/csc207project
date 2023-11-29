5c10211d34faf6ce20bf96a5d9af7748bd2f3bab
Milo <modderme123+school@gmail.com>
2023-11-29 17:27:33 -0500

    ensure tests build

--- tests/ChatTest.java
+++ tests/ChatTest.java
@@ -2,23 +2,30 @@ import static java.lang.Thread.sleep;
 
 import app.ChatUseCaseFactory;
 import data_access.FirebaseMessageDataAccessObject;
+import entity.CommonUserFactory;
+import entity.User;
+import entity.UserFactory;
 import interface_adapter.chat.ChatViewModel;
+
+import java.time.LocalDateTime;
 import java.util.ArrayList;
 import javax.swing.*;
 import okhttp3.OkHttpClient;
 import org.junit.Assert;
 import org.junit.Test;
 import use_case.chat.ChatMessageDataAccessInterface;
+import use_case.chat.ChatUserDataAccessInterface;
 import view.ChatView;
 
 public class ChatTest {
   @Test
   public void displaysMessages() {
     OkHttpClient client = new OkHttpClient();
+    User user = new CommonUserFactory().create("test-user-id", "example@example.com","test-user-name", "password", LocalDateTime.now());
     ChatMessageDataAccessInterface messageDataAccessObject =
         new FirebaseMessageDataAccessObject(client);
     var chatViewModel = new ChatViewModel(new ArrayList<>());
-    ChatView view = ChatUseCaseFactory.create(messageDataAccessObject, chatViewModel);
+    ChatView view = ChatUseCaseFactory.create(messageDataAccessObject, chatViewModel, () -> user);
 
     JFrame jf = new JFrame();
     jf.setContentPane(view.contentPane);
