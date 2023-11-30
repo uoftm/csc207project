6b86a186affc3b1bd7657d914f16c27cb2c21691
Alex <alexanderrosen45@gmail.com>
2023-11-29 23:31:36 -0500

    Switch view presenter changes

--- src/interface_adapter/settings/SettingsPresenter.java
+++ src/interface_adapter/settings/SettingsPresenter.java
@@ -5,7 +5,7 @@ import use_case.settings.SettingsOutputData;
 import use_case.switch_view.SwitchViewOutputBoundary;
 import use_case.switch_view.SwitchViewOutputData;
 
-public class SettingsPresenter implements SettingsOutputBoundary, SwitchViewOutputBoundary {
+public class SettingsPresenter implements SettingsOutputBoundary {
 
   private final SettingsViewModel settingsViewModel;
 
@@ -23,10 +23,5 @@ public class SettingsPresenter implements SettingsOutputBoundary, SwitchViewOutp
   public void prepareFailView(String error) {
     // On error, switch back to the same view for now
     System.out.println("error");
-  }
-
-  @Override
-  public void present(SwitchViewOutputData outputData) {
-    // Logic to handle view switching
   }
 }
